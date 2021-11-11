91141b07ecebc27d4b4acfbb8a142650aab7442a
Bagasfrmnsh <bagasfrmnsh13@gmail.com>
2021-11-11 11:05:23 +0700

    author dan buku

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,13 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
-Route::group(['prefix'=>'admin', 'middleware'=>['auth']], function () {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::group(['prefix'=>'admin', 'middleware'=>['auth','role:admin']], function () {
+    Route::get('/', function (){
+        return view('admin.index');
     });
-    
+});
+
 
